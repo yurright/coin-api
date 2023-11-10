ae47b049ca2d4c84d4fc221cb8f7ba21725b57ba
cryptojang <robert.crypto.jang@gmail.com>
2023-11-10 17:43:44 +0900

    slide-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CoinCard from "./components/CoinCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const App = () => {
+  const [coinPrices, setCoinPrices] = useState();
+
+  const getCoinPrices = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.upbit.com/v1/ticker?markets=KRW-BTC,%20KRW-ETH,%20KRW-MATIC,BTC-ASTR"
+      );
+      console.log(response.data);
+      setCoinPrices(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCoinPrices();
+  }, []);
+
   return (
-    <div className="bg-red-100">
-      <div>Hello</div>
+    <div className=" min-h-screen flex flex-col justify-center items-center  ">
+      {coinPrices ? (
+        <ul className="w-96">
+          <Slider
+            autoplay={true}
+            autoplaySpeed={1000}
+            arrows={false}
+            dots={true}
+          >
+            {coinPrices.map((v, i) => {
+              return (
+                <CoinCard
+                  key={i}
+                  coinPrice={v}
+                  btcPrice={coinPrices[0].trade_price}
+                />
+              );
+            })}
+          </Slider>
+        </ul>
+      ) : (
+        <div>로딩중...</div>
+      )}
     </div>
   );
 };
---
+++ src/components/CoinCard.jsx
@@ -0,0 +1,23 @@
+const CoinCard = ({ coinPrice, btcPrice }) => {
+  return (
+    <li className="text-2xl flex items-center justify-center">
+      <img
+        className="w-10 inline-block "
+        src={`./images/${coinPrice.market.substring(4)}.png`}
+        alt={coinPrice.market}
+      />
+      <span className="ml-4 inline-block w-12 ">
+        {" "}
+        {coinPrice.market.substring(4)}
+      </span>
+      <span className="ml-10">
+        ₩
+        {coinPrice.market.substring(0, 3) === "BTC"
+          ? (coinPrice.trade_price * btcPrice).toFixed(2).toLocaleString()
+          : coinPrice.trade_price}{" "}
+      </span>
+    </li>
+  );
+};
+
+export default CoinCard;
